75ee6975c0e29ccf22869c4c3a898974b3345561
TomasCheng <993690377@qq.com>
2016-09-13 00:34:56 +0800

    初步完成客户端编辑

--- ChatClient/Form1.Designer.cs
+++ ChatClient/Form1.Designer.cs
@@ -28,12 +28,189 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbServerIp = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNickName = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUsers = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbSend = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.tbMsgs = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "服务器IP：";
+            // 
+            // tbServerIp
+            // 
+            this.tbServerIp.Location = new System.Drawing.Point(109, 35);
+            this.tbServerIp.Name = "tbServerIp";
+            this.tbServerIp.Size = new System.Drawing.Size(128, 21);
+            this.tbServerIp.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(270, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "昵称：";
+            // 
+            // tbNickName
+            // 
+            this.tbNickName.Location = new System.Drawing.Point(317, 35);
+            this.tbNickName.MaxLength = 8;
+            this.tbNickName.Name = "tbNickName";
+            this.tbNickName.Size = new System.Drawing.Size(123, 21);
+            this.tbNickName.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(505, 35);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "登录";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(503, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "在线用户：";
+            // 
+            // tbUsers
+            // 
+            this.tbUsers.Location = new System.Drawing.Point(505, 102);
+            this.tbUsers.Multiline = true;
+            this.tbUsers.Name = "tbUsers";
+            this.tbUsers.ReadOnly = true;
+            this.tbUsers.Size = new System.Drawing.Size(119, 282);
+            this.tbUsers.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "聊天内容：";
+            // 
+            // tbSend
+            // 
+            this.tbSend.Location = new System.Drawing.Point(46, 406);
+            this.tbSend.Multiline = true;
+            this.tbSend.Name = "tbSend";
+            this.tbSend.Size = new System.Drawing.Size(534, 82);
+            this.tbSend.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(598, 406);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 82);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(44, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "状态：";
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(107, 9);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(29, 12);
+            this.lbStatus.TabIndex = 12;
+            this.lbStatus.Text = "离线";
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(598, 35);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(75, 23);
+            this.btnQuit.TabIndex = 13;
+            this.btnQuit.Text = "下线";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            // 
+            // tbMsgs
+            // 
+            this.tbMsgs.Location = new System.Drawing.Point(46, 102);
+            this.tbMsgs.Multiline = true;
+            this.tbMsgs.Name = "tbMsgs";
+            this.tbMsgs.ReadOnly = true;
+            this.tbMsgs.Size = new System.Drawing.Size(397, 282);
+            this.tbMsgs.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(685, 500);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbSend);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbMsgs);
+            this.Controls.Add(this.tbUsers);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbNickName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbServerIp);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "网络聊天室V1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbServerIp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNickName;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbUsers;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbSend;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.TextBox tbMsgs;
     }
 }
 
--- ChatClient/Form1.cs
+++ ChatClient/Form1.cs
@@ -5,16 +5,120 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ChatClient
 {
     public partial class Form1 : Form
     {
+        static TcpClient client = null;
+        static NetworkStream stream = null;
+        static string ServerIP = null;
+        static string NickName = null;
+        static int port = 9999;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnLogin.Enabled = true;
+            btnQuit.Enabled = false;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if(tbServerIp.Text=="")
+            {
+                MessageBox.Show("服务器IP不能为空！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            if (tbNickName.Text == "")
+            {
+                MessageBox.Show("昵称不能为空！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            ServerIP = tbServerIp.Text;
+            NickName = tbNickName.Text;
+            client = new TcpClient();
+            client.Connect(ServerIP,port);
+            lbStatus.Text = NickName + "（在线）";
+            ThreadsStart();
+            
+        }
+        public void ThreadsStart()
+        {
+            Thread myThread = new Thread(new ThreadStart(GetMsgs));
+            myThread.Start();
+        }
+        private void GetMsgs()
+        {
+            while(true)
+            {
+                Byte[] bytes = new Byte[1024];
+                stream = client.GetStream();
+                stream.Read(bytes, 0, bytes.Length);
+                string data = Encoding.UTF8.GetString(bytes, 0, bytes.Length);
+                //对获取到的信息进行解析
+                //U:add:name1
+                //U:remove:name2
+                string[] datas = data.Split(':');
+                if(datas[0].Equals("U"))    //用户列表变化消息
+                {
+                    if(datas[1].Equals("add"))
+                    {
+                        tbUsers.Text += datas[2] + "\n";
+                    }
+                    else if(datas[1].Equals("remove"))
+                    {
+                        string[] users = tbUsers.Text.Split('\n');
+                        for(int i=0;i<users.Length;i++)
+                        {
+                            if(users[i].Equals(datas[2]))
+                            {
+                                users[i] = "";
+                                break;
+                            }
+                        }
+                        for (int i = 0; i < users.Length; i++)
+                        {
+                            tbUsers.Text += users[i] + "\n";
+                        }
+
+                    }
+                }
+                //M:time:name3:content
+                else if(datas[0].Equals("M"))   //消息变化消息
+                {
+                    tbMsgs.Text += "Time:" + datas[1] + "\n";
+                    tbMsgs.Text += datas[2] + ":" + datas[3] + "\n\n";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(tbSend.Text=="")
+            {
+                MessageBox.Show("请输入要发送的内容！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            //对即将发送的消息的包装
+            StringBuilder sb = new StringBuilder();
+            sb.Append("M:");
+            sb.Append(new DateTime().ToLocalTime() + ":");
+            sb.Append(NickName + ":");
+            sb.Append(tbSend.Text);
+
+            byte[] msg = Encoding.UTF8.GetBytes(sb.ToString());
+            stream = client.GetStream();
+            stream.Write(msg, 0, msg.Length);
         }
     }
 }
